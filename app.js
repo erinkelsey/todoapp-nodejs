28d2c64ebaf063ede5928dccf99d2d0ba0702061
erinkelsey <erinszilagyi@gmail.com>
2020-09-30 23:16:32 -0600

    add work list

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-let items = [];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,12 +19,21 @@ app.get("/", (req, res) => {
   };
 
   const currentDateString = today.toLocaleDateString("en-US", options);
-  res.render("list", { currentDay: currentDateString, listItems: items });
+  res.render("list", { listTitle: currentDateString, listItems: items });
 });
 
 app.post("/", (req, res) => {
-  items.push(req.body.newItem);
-  res.redirect("/");
+  if (req.body.list === "Work List") {
+    workItems.push(req.body.newItem);
+    res.redirect("/work");
+  } else {
+    items.push(req.body.newItem);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", listItems: workItems });
 });
 
 app.listen(3000, () => {
